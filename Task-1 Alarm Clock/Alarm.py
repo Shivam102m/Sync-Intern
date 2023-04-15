df97193f47cad1fadcef6452bb963f942f3acefe
Shivam Morey <95848163+Shivam102m@users.noreply.github.com>
2023-04-15 14:49:45 +0530

    Add files via upload

---
+++ Task-1 Alarm Clock/Alarm.py
@@ -0,0 +1,38 @@
+import datetime
+import tkinter as tk
+import tkinter.simpledialog
+
+class AlarmClock:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Alarm Clock")
+        self.current_time_label = tk.Label(self.master, font=("Arial", 24))
+        self.current_time_label.pack(pady=20)
+        self.set_alarm_button = tk.Button(self.master, text="Set Alarm", command=self.set_alarm)
+        self.set_alarm_button.pack(pady=10)
+        self.alarm_time_label = tk.Label(self.master, font=("Arial", 24))
+        self.alarm_time_label.pack(pady=20)
+        self.alarm_set = False
+        self.alarm_time = None
+        self.check_alarm()
+
+    def set_alarm(self):
+        self.alarm_set = True
+        self.alarm_time = tkinter.simpledialog.askstring("Set Alarm", "Enter time in HH:MM:SS format:")
+        self.alarm_time_label.config(text=f"Alarm set for {self.alarm_time}")
+
+    def check_alarm(self):
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        self.current_time_label.config(text=f"Current time: {current_time}")
+        if self.alarm_set and current_time == self.alarm_time:
+            tk.messagebox.showinfo("Alarm", "Time to wake up!")
+            self.alarm_set = False
+            self.alarm_time = None
+            self.alarm_time_label.config(text="")
+
+        self.master.after(1000, self.check_alarm)
+
+root = tk.Tk()
+root.geometry('500x234')
+alarm_clock = AlarmClock(root)
+root.mainloop()
